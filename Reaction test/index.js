b6a53d968d18c18abf4b3590ac91f6da18f701d2
mojtaba nabilo <mojjtaba00@gmail.com>
2022-02-12 21:13:33 +0330

    added h1

--- Reaction test/index.js
+++ Reaction test/index.js
@@ -42,13 +42,17 @@ button.addEventListener("click" , () => {
         if(X === true){
             let v = setInterval(timer2, 10);
             backGround.addEventListener("click", () => {
-            const result = document.createElement("p");
-            result.classList.add("result-reaction");
-            result.style.fontSize = "7rem";
-            result.textContent = `${second2}   :   ${miliSecond2}`;
-            backGround.appendChild(result);
-            clearInterval(v);
-            });
+                const result = document.createElement("p");
+                const h1 = document.createElement("h1");
+                h1.classList.add("title");
+                h1.style.fontSize = "4rem";
+                h1.textContent = "your reaction"
+                backGround.appendChild(h1);
+                result.classList.add("result-reaction");
+                result.style.fontSize = "7rem";
+                result.textContent = `${second2}   :   ${miliSecond2}`;
+                backGround.appendChild(result);
+                clearInterval(v);
         }
     },timesRandomTime);
     setInterval(timer, 10);
